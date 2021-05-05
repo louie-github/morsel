b4ef340d0cfe806428e21a6b46041cd865c2d87d
louie-github <30176969+louie-github@users.noreply.github.com>
2021-05-06 02:21:41 +0800

    Add docstrings, refactor some code, and fix allow_clipping

--- morsel/wav.py
+++ morsel/wav.py
@@ -4,7 +4,8 @@
 import math
 import io
 import itertools
-from typing import Iterable, Iterator
+
+from typing import Iterable
 
 # TODO: Add docstrings, __repr__() methods, and unittests
 
@@ -14,15 +15,21 @@ DEFAULT_BIT_DEPTH = 16
 SUPPORTED_BIT_DEPTHS = {16, 24, 32}
 
 
-def _int_to_field(i: int, length: int) -> None:
-    """Convert int objects to RIFF fields (unsigned little-endian)
+def _int_to_riff(i: int, length: int) -> bytes:
+    """Convert an int to its byte representation in a RIFF file.
+
+    Represents integers as unsigned integers in *length* bytes encoded
+    in little-endian.
 
     Args:
-        i (int): The integer to convert.
-        length (int): The number of bytes used to represent the number.
+        i (int):
+            The integer to represent.
+        length (int):
+            The number of bytes used to represent the integer.
 
     Returns:
-        None
+        bytes:
+            The bytes representation of the specified integer.
     """
     return i.to_bytes(length, "little", signed=False)
 
@@ -34,6 +41,27 @@ def generate_pcm_wave_file_header(
     sample_rate: int = 48000,
     bits_per_sample: int = DEFAULT_BIT_DEPTH,
 ) -> bytes:
+    """Generates a PCM WAVE file header.
+
+    Args:
+        num_samples (int):
+            The number of samples in the PCM data.
+        num_channels (int, optional):
+            The number of channels in the PCM data. Defaults to 2.
+        sample_rate (int, optional):
+            The sample rate of the PCM data. Defaults to 48000.
+        bits_per_sample (int, optional):
+            The bit depth or number of bits per sample in the PCM data.
+            Defaults to DEFAULT_BIT_DEPTH.
+
+    Raises:
+        ValueError:
+            An unsupported bit depth was specified.
+
+    Returns:
+        bytes:
+            The PCM wave file header.
+    """
     if bits_per_sample not in SUPPORTED_BIT_DEPTHS:
         raise ValueError(
             " ".join(
@@ -53,18 +81,18 @@ def generate_pcm_wave_file_header(
     # Set RIFF fields
     # fmt: off
     chunk_id        = b"RIFF"
-    chunk_size      = _int_to_field(chunk_size, 4)
+    chunk_size      = _int_to_riff(chunk_size, 4)
     format_         = b"WAVE"  # format_ to avoid conflict with builtin "format"
     subchunk_1_id   = b"fmt "
-    subchunk_1_size = _int_to_field(16, 4)
-    audio_format    = _int_to_field(1, 2)  # PCM = 1
-    num_channels    = _int_to_field(num_channels, 2)
-    sample_rate     = _int_to_field(sample_rate, 4)
-    byte_rate       = _int_to_field(byte_rate, 4)
-    block_align     = _int_to_field(block_align, 2)
-    bits_per_sample = _int_to_field(bits_per_sample, 2)
+    subchunk_1_size = _int_to_riff(16, 4)
+    audio_format    = _int_to_riff(1, 2)  # PCM = 1
+    num_channels    = _int_to_riff(num_channels, 2)
+    sample_rate     = _int_to_riff(sample_rate, 4)
+    byte_rate       = _int_to_riff(byte_rate, 4)
+    block_align     = _int_to_riff(block_align, 2)
+    bits_per_sample = _int_to_riff(bits_per_sample, 2)
     subchunk_2_id   = b"data"
-    subchunk_2_size = _int_to_field(subchunk_2_size, 4)
+    subchunk_2_size = _int_to_riff(subchunk_2_size, 4)
     # fmt: on
     return b"".join(
         (
@@ -85,7 +113,22 @@ def generate_pcm_wave_file_header(
     )
 
 
-def _map_floats_to_ints(floats: Iterable[float], bits_per_sample: int):
+def _map_floats_to_ints(floats: Iterable[float], bits_per_sample: int) -> Iterable[int]:
+    """Maps float values in the range -1.0 to 1.0 to integer values.
+
+    The integer range is determined by the bits_per_sample value. The
+    range is defined as the minimum and maximum values representable
+    using signed integers of size bits_per_sample.
+
+    Args:
+        floats (Iterable[float]):
+            The float values to map to integers.
+        bits_per_sample (int):
+            The bit depth used to determine the output integer range.
+
+    Returns:
+        Iterable[int]: The mapped integer values.
+    """
     max_signed = 2 ** (bits_per_sample - 1) - 1
     min_signed = ~max_signed
     # Every x increase from the minimum float value maps to this value
@@ -94,9 +137,62 @@ def _map_floats_to_ints(floats: Iterable[float], bits_per_sample: int):
     return (round((value - (-1.0)) * step_size + min_signed) for value in floats)
 
 
+def _clamp_floats(floats: Iterable[float]):
+    return (max(min(value, 1.0), -1.0) for value in floats)
+
+
+def _ints_to_pcm_wave(
+    int_data: Iterable[int], *, bits_per_sample: int, num_channels: int
+) -> Iterable[bytes]:
+    """
+    Converts raw integer data to PCM data with interleaved channels.
+
+    If num_channels is given, int_data will be repeated and used to
+    fill all channels, so each data point will be repeated num_channels
+    times (each sample has interleaved channel data).
+
+    Args:
+        int_data (Iterable[int]):
+            The raw integer data.
+        bits_per_sample (int):
+            The bit depth of the PCM data.
+        num_channels (int):
+            The number of channels in the PCM data.
+
+    Returns:
+        Iterable[bytes]:
+            An iterable with the resulting PCM data.
+    """
+    bytes_per_sample = bits_per_sample // 8
+    output = (i.to_bytes(bytes_per_sample, "little", signed=True) for i in int_data)
+    output = (
+        data for value in output for data in itertools.repeat(value, num_channels)
+    )
+    return output
+
+
 def _generate_sine_wave(
     *, angular_frequency: float, amplitude: float, num_samples: int, sample_rate: int
-):
+) -> Iterable[float]:
+    """Generates samples of a sine wave function for PCM data.
+
+    Args:
+        angular_frequency (float):
+            The angular frequency of the function or rate of change in
+            radians per second.
+        amplitude (float):
+            The amplitude of the function or the maximum deviation from
+            zero.
+        num_samples (int):
+            The number of samples to generate.
+        sample_rate (int):
+            The sample rate of the generated data, in cycles per second
+            or Hertz (Hz).
+
+    Returns:
+        Iterable[float]:
+            The generated samples of the sine wave function.
+    """
     return (
         amplitude * math.sin(angular_frequency * t / sample_rate)
         for t in range(num_samples)
@@ -112,7 +208,46 @@ def generate_sine_wave(
     sample_rate: int = 48000,
     bits_per_sample: int = DEFAULT_BIT_DEPTH,
     allow_clipping=True,
-) -> Iterator:
+) -> Iterable[bytes]:
+    """Generates PCM data for a sine wave.
+
+    If allow_clipping = True, then any floating point values generated
+    in the inner sine wave function that fall outside the range
+    -1.0 to 1.0 will be clamped to either -1.0 or 1.0, whichever is
+    appropriate.
+
+    Args:
+        frequency (int, optional):
+            The frequency of the sine wave in cycles per second or
+            Hertz (Hz). Defaults to 500.
+        amplitude (float, optional):
+            The amplitude of the sine wave or maximum deviation from
+            zero. Defaults to 0.75.
+        num_samples (int, optional):
+            The number of samples to generate. Defaults to 48000.
+        num_channels (int, optional):
+            The number of channels of the PCM data. Defaults to 2.
+        sample_rate (int, optional):
+            The sample rate of the PCM data. Defaults to 48000.
+        bits_per_sample (int, optional):
+            The bit depth / number of bits per sample in the PCM data.
+            Defaults to DEFAULT_BIT_DEPTH.
+        allow_clipping (bool, optional):
+            Whether to allow clipping of output values. Defaults to True.
+
+    Raises:
+        ValueError:
+            An unsupported bit depth was specified.
+        ValueError:
+            An amplitude smaller than 0.0 was specified.
+        ValueError:
+            An amplitude larger than 1.0 was specified while clipping
+            was not allowed.
+
+    Returns:
+        Iterable[bytes]:
+            An iterable of the PCM data of the sine wave.
+    """
     if bits_per_sample not in SUPPORTED_BIT_DEPTHS:
         raise ValueError(
             " ".join(
@@ -150,14 +285,11 @@ def generate_sine_wave(
         num_samples=min_samples,
         sample_rate=sample_rate,
     )
-    # Convert raw float data to integer PCM data with interleaved channels
+    if allow_clipping:
+        exact_cycle = _clamp_floats(exact_cycle)
     exact_cycle = _map_floats_to_ints(exact_cycle, bits_per_sample=bits_per_sample)
-    bytes_per_sample = bits_per_sample // 8
-    exact_cycle = (
-        i.to_bytes(bytes_per_sample, "little", signed=True) for i in exact_cycle
-    )
-    exact_cycle = (
-        data for value in exact_cycle for data in itertools.repeat(value, num_channels)
+    exact_cycle = _ints_to_pcm_wave(
+        exact_cycle, bits_per_sample=bits_per_sample, num_channels=num_channels
     )
     output = itertools.islice(itertools.cycle(exact_cycle), num_samples * num_channels)
     return output
@@ -175,6 +307,37 @@ def generate_sine_wave_file(
     bits_per_sample: int = DEFAULT_BIT_DEPTH,
     allow_clipping=True,
 ) -> int:
+    """Generate a PCM WAVE (.wav) file containing a sine wave.
+
+    Args:
+        filename (str):
+            The filename of the output file.
+        buffer_size (int, optional):
+            The number of bytes to write to the file at a time.
+            Defaults to 8192.
+        frequency (int, optional):
+            The frequency of the sine wave in cycles per second or
+            Hertz (Hz). Defaults to 500.
+        amplitude (float, optional):
+            The amplitude of the sine wave or maximum deviation from
+            zero. Defaults to 0.75.
+        num_samples (int, optional):
+            The number of samples to generate. Defaults to 48000.
+        num_channels (int, optional):
+            The number of channels of the PCM data. Defaults to 2.
+        sample_rate (int, optional):
+            The sample rate of the PCM data. Defaults to 48000.
+        bits_per_sample (int, optional):
+            The bit depth / number of bits per sample in the PCM data.
+            Defaults to DEFAULT_BIT_DEPTH.
+        allow_clipping (bool, optional):
+            Whether to allow clipping of output values. Defaults to True.
+
+
+    Returns:
+        int:
+            The number of bytes written.
+    """
     header = generate_pcm_wave_file_header(
         num_samples=num_samples,
         num_channels=num_channels,
@@ -209,11 +372,14 @@ def generate_sine_wave_file(
     return bytes_written
 
 
+# TODO: Add generate_silence
+
+
 if __name__ == "__main__":
     import time
 
     BUFSIZE = io.DEFAULT_BUFFER_SIZE
-    filename = "test-5min-512hz-sr48khz-s24le.wav"
+    filename = "test-5min-512hz-sr48khz-s24le-3.wav"
     frequency = 512
     sample_rate = 48000
     duration = 5 * 60 * sample_rate  # 5 minutes
